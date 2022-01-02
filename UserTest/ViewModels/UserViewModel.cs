50fb1cc5cd821860b68c4b92a6302fe817459104
Mamaedishe <sharifkulov.marat@mail.ru>
2022-01-02 18:27:51 +0300

    Добавьте файлы проекта.

---
+++ UserTest/ViewModels/UserViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UserTest.ViewModels
+{
+    class UserViewModel:DependencyObject
+    {
+
+
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(UserViewModel), new PropertyMetadata(0));
+
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(UserViewModel), new PropertyMetadata(0));
+
+
+
+
+    }
+}
